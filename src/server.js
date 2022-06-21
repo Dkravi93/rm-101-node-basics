233cb24cb41f5e219731424891b4198882c4d4a5
DeepakRavi <94233440+Dkravi93@users.noreply.github.com>
2022-06-21 23:48:52 +0530

    done

--- src/server.js
+++ src/server.js
@@ -1,8 +1,37 @@
 // install and import express
-const express = () => {};
+const express = require('express');
 let app = express();
+const path = require('path');
+// import user from "./assets/user.json"  
+const user = require("./assets/user.json")
 
 // Code here
 
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname+'assets','users.html'));
+})
+
+app.get('/users', (req,res) => {
+    res.json(user);
+});
+
+app.get('/users/:id', (req, res) => {
+  let dev = req.params.id;
+  function check(data) {
+    return data.id == dev;
+  }
+   const person = user.find(check);
+   
+   res.json(person);
+})
+app.post('users', (req,res) =>{
+  const updatedUser = user.assign(req.body);
+  res.json(updatedUser);
+})
+
+
+app.listen(8000);
+console.log("Running at Port 8000");
 // Note: Do not remove this export statement
 module.exports = app;
